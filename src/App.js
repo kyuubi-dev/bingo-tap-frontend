b2aff52a41d4bdeb7103616ed2df298037dc0849
kraievskyi <bogdan.1233211233213@gmail.com>
2024-06-09 00:10:07 +0300

    loading screen adding 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Tap from './Pages/Tap';
 import BgImage from './Pages/BgImage';
@@ -6,28 +6,87 @@ import Team from './Pages/Team';
 import Navigation from './Pages/Navigation';
 import Task from './Pages/Task';
 import Boost from './Pages/Boost';
+import LoadingScreen from './Pages/LoadingScreen.js';
 import Stat from './Pages/Stat';
+
 function App() {
-  const [userBalance, setUserBalance] = useState(0);
-  document.addEventListener('gesturestart', function (e) {
-    e.preventDefault();
-});
-  return (
-    <div className="App">
-      <BgImage/>
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          
-          <Route path="/" element={<Tap setUserBalance={setUserBalance} />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/task" element={<Task userBalance={userBalance} />} />
-          <Route path="/boost" element={<Boost />} />
-          <Route path='/stat' element={<Stat/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    const [userBalance, setUserBalance] = useState(0);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    document.addEventListener('gesturestart', function (e) {
+        e.preventDefault();
+    });
+
+    useEffect(() => {
+        const loadResources = async () => {
+            const images = [
+                '/coin.png',
+                '/ranks/gold.png',
+                './ranks/blue.png',
+                './ranks/neon.png',
+                './ranks/green.png',
+                './tasks/task.png',
+                './tasks/open.png',
+                './tasks/people1.png',
+                './tasks/people2.png',
+                './tasks/people3.png',
+                './boost/power.png',
+                './btns/robotv2.png',
+                './boost/click.png',
+                './boost/dow.png',
+                './boost/fire.b.png',
+                './boost/fire.p.png',
+                './btns/boost.png',
+                './btns/boost_active.png',
+                './btns/stat.png',
+                './btns/stat_active.png',
+                './btns/tap.png',
+                './btns/tap_active.png',
+                './btns/task.png',
+                './btns/task_active.png',
+                './btns/team.png',
+                './btns/team_active.png'
+            ];
+
+            const promises = images.map((src) => {
+                return new Promise((resolve, reject) => {
+                    const img = new Image();
+                    img.src = src;
+                    img.onload = resolve;
+                    img.onerror = reject;
+                });
+            });
+
+            try {
+                await Promise.all(promises);
+                setIsLoaded(true);
+            } catch (error) {
+                console.error('Ошибка при загрузке ресурсов:', error);
+            }
+        };
+
+        loadResources();
+    }, []);
+
+    if (!isLoaded) {
+        return <LoadingScreen />;
+    }
+
+    return (
+        <div className="App">
+            <BgImage />
+            <BrowserRouter>
+                <Navigation />
+                <Routes>
+                    <Route path="/" element={<Tap setUserBalance={setUserBalance} />} />
+                    <Route path="/team" element={<Team />} />
+                    <Route path="/task" element={<Task userBalance={userBalance} />} />
+                    <Route path="/boost" element={<Boost />} />
+                    <Route path='/stat' element={<Stat />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/Pages/Task.js
+++ src/Pages/Task.js
@@ -1,106 +1,155 @@
 import './Task.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './TextStyle.css';
+import { useLocation } from 'react-router-dom';
 
-const Task = ({ userBalance }) => {
-  const [selectedTab, setSelectedTab] = useState('special');
-  const handleTabClick = (tab) => {
-    setSelectedTab(tab);
-  };
+const Task = () => {
+    const location = useLocation();
+    const initialBalance = location.state?.userBalance || 0;
+    const query = new URLSearchParams(location.search);
+    const defaultTab = query.get('tab') || 'special';
 
-  const handleGoldButtonClick = () => {
-    setSelectedTab('leagues'); // Відкриваємо вкладку "LEAGUES" при натисканні на кнопку GOLD
-  };
+    const [userBalance, setUserBalance] = useState(initialBalance);
+    const [selectedTab, setSelectedTab] = useState(defaultTab);
+    const [tasksCompleted, setTasksCompleted] = useState(
+        JSON.parse(localStorage.getItem('tasksCompleted')) || {}
+    );
+    const [completionMessage, setCompletionMessage] = useState('');
 
-  const renderContent = () => {
-    switch (selectedTab) {
-      case 'special':
-        return (
-          <div className="tasks">
-            <TaskItem text="JOIN OUR X (TWITTER)" reward="500 000" />
-            <TaskItem text="JOIN OUR TELEGRAM" reward="250 000" />
-            <TaskItem text="JOIN OUR YOUTUBE" reward="250 000" />
-          </div>
-        );
-      case 'leagues':
-        return (
-          <div className="tasks">
-            <LeagueItem text="SILVER" reward="5 000 000" image='./ranks/blue.png' />
-            <LeagueItem text="GOLD" reward="10 000 000" image="./ranks/gold.png" />
-            <LeagueItem text="DIAMOND" reward="50 000 000" image="./ranks/neon.png" />
-          </div>
-        );
-      case 'ref-tasks':
-        return (
-          <div className="tasks">
-            <InviteFriendItem text="INVITE 3 FRIENDS" reward="50 000" image='./tasks/people1.png' />
-            <InviteFriendItem text="INVITE 10 FRIENDS" reward="100 000" image='./tasks/people2.png'/>
-            <InviteFriendItem text="INVITE 25 FRIENDS" reward="500 000" image='./tasks/people3.png'/>
-            <InviteFriendItem text="INVITE 100 FRIENDS" reward="1 000 000" image='./tasks/people3.png' />
-            <InviteFriendItem text="INVITE 1000 FRIENDS" reward="5 000 000" image='./tasks/people3.png'/>
-            <InviteFriendItem text="INVITE 10 000 FRIENDS" reward="10 000 000" image='./tasks/people3.png'/>
-          </div>
-        );
-      default:
-        return null;
-    }
-  };
+    useEffect(() => {
+        setSelectedTab(defaultTab);
+    }, [defaultTab]);
 
-  return (
-    <div className="Task">
-      <div className="bg-image" />
-      <header className="header">
-        <div className="balance-display-task">
-          <img src="/coin.png" alt="Coin" className="coin-icon" />
-          <span className=" blue-style">{userBalance}</span>
-        </div>
-        <div className="gold" onClick={handleGoldButtonClick}>
-          <img src='./ranks/gold.png' className='rank-img' alt="Gold Rank" />
-          <span className="gold-style">GOLD</span>
-          <button className='open-btn'>
-            <img src='./tasks/open.png' className='open-icon' alt="Open" />
-          </button>
-        </div>
-      </header>
-      
-      <nav className="nav">
-        <div 
-          className={`tab ${selectedTab === 'special' ? 'selected' : ''}`} 
-          onClick={() => handleTabClick('special')}
-        >
-          SPECIAL
-        </div>
-        <div 
-          className={`tab ${selectedTab === 'leagues' ? 'selected' : ''}`} 
-          onClick={() => handleTabClick('leagues')}
-        >
-          LEAGUES
-        </div>
-        <div 
-          className={`tab ${selectedTab === 'ref-tasks' ? 'selected' : ''}`} 
-          onClick={() => handleTabClick('ref-tasks')}
-        >
-          REF TASKS
-        </div>
-      </nav>
+    const handleTabClick = (tab) => {
+        setSelectedTab(tab);
+    };
 
-      {renderContent()}
-    </div>
-  );
+    const handleGoldButtonClick = () => {
+        setSelectedTab('leagues');
+    };
+
+    const handleTaskCompletion = (taskName, reward) => {
+        const updatedTasksCompleted = { ...tasksCompleted, [taskName]: true };
+        setTasksCompleted(updatedTasksCompleted);
+        localStorage.setItem('tasksCompleted', JSON.stringify(updatedTasksCompleted));
+        setUserBalance(userBalance + reward);
+        setCompletionMessage(`Задание выполнено: ${taskName}, награда - ${reward}`);
+    };
+
+    const renderContent = () => {
+        switch (selectedTab) {
+            case 'special':
+                return (
+                    <div className="tasks">
+                        {!tasksCompleted['JOIN OUR X (TWITTER)'] && (
+                            <TaskItem
+                                text="JOIN OUR X (TWITTER)"
+                                reward={500000}
+                                url="https://twitter.com"
+                                onCompletion={() => handleTaskCompletion('JOIN OUR X (TWITTER)', 500000)}
+                            />
+                        )}
+                        {!tasksCompleted['JOIN OUR TELEGRAM'] && (
+                            <TaskItem
+                                text="JOIN OUR TELEGRAM"
+                                reward={250000}
+                            />
+                        )}
+                        {!tasksCompleted['JOIN OUR YOUTUBE'] && (
+                            <TaskItem
+                                text="JOIN OUR YOUTUBE"
+                                reward={250000}
+                            />
+                        )}
+                    </div>
+                );
+            case 'leagues':
+                return (
+                    <div className="tasks">
+                        <LeagueItem text="SILVER" reward="5 000 000" image='./ranks/blue.png' />
+                        <LeagueItem text="GOLD" reward="10 000 000" image="./ranks/gold.png" />
+                        <LeagueItem text="DIAMOND" reward="50 000 000" image="./ranks/neon.png" />
+                    </div>
+                );
+            case 'ref-tasks':
+                return (
+                    <div className="tasks">
+                        <InviteFriendItem text="INVITE 3 FRIENDS" reward="50 000" image='./tasks/people1.png' />
+                        <InviteFriendItem text="INVITE 10 FRIENDS" reward="100 000" image='./tasks/people2.png'/>
+                        <InviteFriendItem text="INVITE 25 FRIENDS" reward="500 000" image='./tasks/people3.png'/>
+                        <InviteFriendItem text="INVITE 100 FRIENDS" reward="1 000 000" image='./tasks/people3.png' />
+                        <InviteFriendItem text="INVITE 1000 FRIENDS" reward="5 000 000" image='./tasks/people3.png'/>
+                        <InviteFriendItem text="INVITE 10 000 FRIENDS" reward="10 000 000" image='./tasks/people3.png'/>
+                    </div>
+                );
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <div className="Task">
+            <div className="bg-image" />
+            <header className="header">
+                <div className="balance-display-task">
+                    <img src="/coin.png" alt="Coin" className="coin-icon" />
+                    <span className="blue-style">{userBalance}</span>
+                </div>
+                <div className="gold" onClick={handleGoldButtonClick}>
+                    <img src='./ranks/gold.png' className='rank-img' alt="Gold Rank" />
+                    <span className="gold-style">GOLD</span>
+                    <button className='open-btn'>
+                        <img src='./tasks/open.png' className='open-icon' alt="Open" />
+                    </button>
+                </div>
+            </header>
+
+            <nav className="nav">
+                <div
+                    className={`tab ${selectedTab === 'special' ? 'selected' : ''}`}
+                    onClick={() => handleTabClick('special')}
+                >
+                    SPECIAL
+                </div>
+                <div
+                    className={`tab ${selectedTab === 'leagues' ? 'selected' : ''}`}
+                    onClick={() => handleTabClick('leagues')}
+                >
+                    LEAGUES
+                </div>
+                <div
+                    className={`tab ${selectedTab === 'ref-tasks' ? 'selected' : ''}`}
+                    onClick={() => handleTabClick('ref-tasks')}
+                >
+                    REF TASKS
+                </div>
+            </nav>
+
+            {completionMessage && <div className="completion-message">{completionMessage}</div>}
+
+            {renderContent()}
+        </div>
+    );
 };
 
-const TaskItem = ({ text, reward }) => (
-  <div className="task-item">
-    <img src='./tasks/task.png' alt="icon" className="task-icon" />
-    <div className="task-text blue-style">{text}</div>
-    <div className="task-reward">
-      <img src='./coin.png' alt="coin" className="reward-icon" />
-      <span className='rew-text gold-style'>{reward}</span>
-    </div>
-    <button className='open-btn'>
-      <img src='./tasks/open.png' className='open-icon' alt="Open" />
-    </button>
-  </div>
+const TaskItem = ({ text, reward, url, onCompletion }) => (
+    <a
+        href={url}
+        className="task-item"
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={() => onCompletion && onCompletion()}
+    >
+        <img src='./tasks/task.png' alt="icon" className="task-icon" />
+        <div className="task-text blue-style">{text}</div>
+        <div className="task-reward">
+            <img src='./coin.png' alt="coin" className="reward-icon" />
+            <span className='rew-text gold-style'>{reward}</span>
+        </div>
+        <button className='open-btn'>
+            <img src='./tasks/open.png' className='open-icon' alt="Open" />
+        </button>
+    </a>
 );
 
 const LeagueItem = ({ text, reward, image }) => (
@@ -117,14 +166,16 @@ const LeagueItem = ({ text, reward, image }) => (
         <button className="claim-button blue-style">CLAIM</button>
     </div>
 );
+
 const InviteFriendItem = ({text, reward, image}) => (
     <div className="task-item">
         <img src={image} alt="icon" className="task-icon" />
-      <div className="task-text blue-style">{text}</div>
-      <div className="task-reward">
-        <img src='./coin.png' alt="coin" className="reward-icon" />
-        <span className='gold-style'>{reward}</span>
-      </div>
+        <div className="task-text blue-style">{text}</div>
+        <div className="task-reward">
+            <img src='./coin.png' alt="coin" className="reward-icon" />
+            <span className='gold-style'>{reward}</span>
+        </div>
     </div>
-  );
+);
+
 export default Task;
