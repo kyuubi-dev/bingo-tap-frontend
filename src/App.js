1cae1c168639fc22aee42f100269ea54ca11adf0
Artem <139074869+kyuubi-dev@users.noreply.github.com>
2024-06-20 16:48:44 +0300

    fix user_id

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Tap from './Pages/Tap';
 import BgImage from './Pages/BgImage';
 import Team from './Pages/Team';
@@ -13,26 +13,14 @@ import NotMobile from './Pages/NotMobile';
 import config from './config';
 
 function App() {
+    const [userBalance, setUserBalance] = useState(0);
     const [purchasedBoosts, setPurchasedBoosts] = useState({});
     const [isLoaded, setIsLoaded] = useState(false);
-
-    
-    const [loadingImages, setLoadingImages] = useState(true);
-    const location = useLocation();
-
-    const imageSources = {
-        '/': ['./robot.png', './coin.png','/16.png', '/17.png','./ranks/blue.png','./ranks/gold.png','./ranks/neon.png','./ranks/green.png','./boost/power.png','./tasks/open.png'],
-        '/task': ['/16.png', '/17.png','/coin.png','./tasks/open.png','./tasks/people1.png','./tasks/people2.png','./tasks/people3.png','./ranks/blue.png','./ranks/gold.png','./ranks/neon.png','./ranks/green.png'],
-        '/boost': ['/16.png', '/17.png','/coin.png','/boost/fire.b.png','/boost/power.png']
-    };
     const [userId, setUserId] = useState(null);
-
 
     useEffect(() => {
         const fetchUserData = async () => {
             try {
-
-
                 // Получаем обновления через API Telegram
                 const updatesResponse = await axios.get(`https://api.telegram.org/bot${config.telegramBotToken}/getUpdates`);
                 const updates = updatesResponse.data.result;
@@ -64,43 +52,13 @@ function App() {
                 setIsLoaded(true);
             } catch (error) {
                 console.error('Ошибка при получении данных пользователя:', error);
-                setIsLoaded(true); // Allow the app to render despite the error
             }
         };
 
         fetchUserData();
     }, []);
 
-    useEffect(() => {
-        setLoadingImages(true);
-
-        const loadImage = (src) => {
-            return new Promise((resolve, reject) => {
-                const img = new Image();
-                img.src = src;
-                img.onload = resolve;
-                img.onerror = () => {
-                    console.error(`Failed to load image: ${src}`);
-                    reject();
-                };
-            });
-        };
-
-        const loadImagesForRoute = async (route) => {
-            const sources = imageSources[route] || [];
-            try {
-                await Promise.all(sources.map(src => loadImage(src)));
-                setLoadingImages(false);
-            } catch (error) {
-                console.error('Error loading images:', error);
-                setLoadingImages(false);
-            }
-        };
-
-        loadImagesForRoute(location.pathname);
-    }, [location.pathname]);
-
-    if (!isLoaded || loadingImages) {
+    if (!isLoaded) {
         return <LoadingScreen />;
     }
 
@@ -109,12 +67,15 @@ function App() {
             <BgImage />
             <Navigation />
             <Routes>
-
-                <Route path="/" element={<Tap telegramId={telegramId} />} />
+                <Route path="/" element={<Tap telegramId={userId} setUserBalance={setUserBalance} />} />
                 <Route path="/team" element={<Team />} />
-                <Route path="/task" element={<Task telegramId={telegramId} />} />
-                <Route path="/boost" element={<Boost telegramId={telegramId}
-                                                     purchasedBoosts={purchasedBoosts}
+                <Route path="/task" element={<Task telegramId={userId} />} />
+                <Route
+                    path="/boost"
+                    element={
+                        <Boost
+                            telegramId={userId}
+                            purchasedBoosts={purchasedBoosts}
                             setPurchasedBoosts={setPurchasedBoosts}
                         />
                     }
