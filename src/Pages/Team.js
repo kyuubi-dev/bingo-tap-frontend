20e91d95aa205dd82597a934805d390fed107afe
Kyuubi <artem.panasiuk.m@gmail.com>
2024-07-02 20:15:43 +0300

    referer b

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -1,17 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Team.css';
 import './TextStyle.css';
+import config from "../config";
 
 function Team({ userId, botName }) {
-    const [copySuccess, setCopySuccess] = useState('');
+    const [copyButtonText, setCopyButtonText] = useState('COPY');
+    const [referralBalance, setReferralBalance] = useState(0);
+    const [referralCount, setReferralCount] = useState(0);
+
+    useEffect(() => {
+        const fetchReferralData = async () => {
+            try {
+                const balanceResponse = await axios.get(`${config.apiBaseUrl}/user-ref_balance/${userId}`);
+                setReferralBalance(balanceResponse.data.referral_balance);
+                
+                const countResponse = await axios.get(`${config.apiBaseUrl}/user-referral_count/${userId}`);
+                setReferralCount(countResponse.data.referral_count);
+            } catch (error) {
+                console.error('Error fetching referral data: ', error);
+            }
+        };
+
+        fetchReferralData();
+    }, [userId]);
 
     const handleCopyClick = () => {
         const inviteLink = `https://t.me/${botName}?start=${userId}`;
         navigator.clipboard.writeText(inviteLink)
             .then(() => {
-                setCopySuccess('Посилання скопійовано!');
+                setCopyButtonText('COPIED');
                 setTimeout(() => {
-                    setCopySuccess('');
+                    setCopyButtonText('COPY');
                 }, 2000);
             })
             .catch(err => {
@@ -19,9 +39,7 @@ function Team({ userId, botName }) {
             });
     };
 
-
-    const inviteLink = `https://t.me/${botName}?start=${userId}`;
-
+    const inviteLink = `https://t.me/${botName}?start=ref_${userId}`;
 
     return (
         <div className="Team">
@@ -31,16 +49,15 @@ function Team({ userId, botName }) {
             </div>
             <div className="Header">
                 <div className='Referral'>
-                    <span className="referral-count blue-style">2</span>
+                    <span className="referral-count blue-style">{referralCount}</span>
                     <div className='ref-text blue-style'>REFERRALS</div>
                 </div>
             </div>
-            <div className="referral-total blue-style">+100 000</div>
+            <div className="referral-total blue-style">{`+${referralBalance}`}</div>
             <div className="Invite-link">
                 <div className="invite-text blue-style">MY INVITE LINK:</div>
                 <a href={inviteLink} className="invite-url">{inviteLink}</a>
-                <button className="copy-button blue-style" onClick={handleCopyClick}>COPY</button>
-                {copySuccess && <div className="copy-success">{copySuccess}</div>}
+                <button className="copy-button blue-style" onClick={handleCopyClick}>{copyButtonText}</button>
             </div>
             <div className="Team-section">
                 <div className="team-header blue-style">MY TEAM:</div>
