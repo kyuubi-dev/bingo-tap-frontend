a7b9358e88bd735e6d2c9a70cee17c68596d1956
kraievskyi <bogdan.1233211233213@gmail.com>
2024-07-04 01:36:33 +0300

    new config server and fix navigation.css

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 const config = {
-    apiBaseUrl: 'https://bingo.pp.ua/api/eyJhbGciOiJIUzI1NiJ9',
+    apiBaseUrl: 'https://bingo-tap.pp.ua/api/eyJhbGciOiJIUzI1NiJ9',
     telegramBotToken: '7208555837:AAF26oAPtwfVIMfOTnUcGHmZepm5QmD6M00',
-    wsBaseUrl: 'https://bingo.pp.ua/ws/eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXI',
-    telegram_id:874423521 
+    wsBaseUrl: 'https://bingo-tap.pp.ua/ws/eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXI',
+    telegram_id:874423521
 };
 
 export default config;
